ad399ff76782e63142ebaefc4483d0afed85a70f
irvinjo <irvin.pereyra.g@gmail.com>
2017-11-29 22:58:21 -0500

    all ok 2

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-tar czf easyio.tar.gz main.js package.json yarn.lock public LICENSE deploy.sh
-scp easyio.tar.gz root@165.227.189.182:~
+tar czf food2molec.tar.gz main.js package.json public deploy.sh
+scp food2molec.tar.gz root@165.227.189.182:~
 
-rm easyio.tar.gz
+rm food2molec.tar.gz
 
 ssh root@165.227.189.182 << 'ENDSSH'
 pm2 stop all
-rm -rf easyio
-mkdir easyio
-tar xf easyio.tar.gz -C easyio
-rm easyio.tar.gz
-cd easyio 
-yarn install
-pm2 start --name easyio main.js
+rm -rf food2molec
+mkdir food2molec
+tar xf food2molec.tar.gz -C easyio
+rm food2molec.tar.gz
+cd food2molec 
+sudo npm install
+pm2 start --name food2molec main.js
 ENDSSH
