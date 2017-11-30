2d6cd151c8261db8062573c09c6294aa14b85b6b
irvinjo <irvin.pereyra.g@gmail.com>
2017-11-29 23:15:23 -0500

    change bluff 1

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tar czf food2molec.tar.gz main.js package.json public deploy.sh
+tar czf food2molec.tar.gz main.js public deploy.sh
 scp food2molec.tar.gz root@165.227.189.182:~
 
 rm food2molec.tar.gz
@@ -12,6 +12,5 @@ mkdir food2molec
 tar xf food2molec.tar.gz -C easyio
 rm food2molec.tar.gz
 cd food2molec 
-sudo npm install
 pm2 start --name food2molec main.js
 ENDSSH
